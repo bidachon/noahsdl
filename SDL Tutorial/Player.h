b2bb7c37d7203710c8129fe94f8d619069812173
bidachon <nicolas.emmanuel.robert@gmail.com>
2019-05-21 20:58:37 -0700

    Added gitignore

--- SDL Tutorial/Player.h
+++ SDL Tutorial/Player.h
@@ -9,6 +9,8 @@ public:
     void load(int x, int y, int width, int height, std::string textureID);
     void draw(SDL_Renderer* renderer);
     void update();
+    //If you don't implement this, Player will be abstract
+    //and you won't be able to instantiate it.
     //void clean();
 };
 
